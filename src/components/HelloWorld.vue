5ece51a5cff35f60e848dad72e46d667428d7beb
Denny Crall <dennis-crall@uiowa.edu>
2020-08-03 09:27:14 -0500

    Add lodash kebabCase to library
    - Add lodash-es as dependency
    - Import single function kebabCase
    - Majority of lodash is dropped in library build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ message }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +31,18 @@
 </template>
 
 <script>
+import { kebabCase } from 'lodash-es'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  computed: {
+    message: function() {
+      return kebabCase(this.msg);
+    }
   }
 }
 </script>
